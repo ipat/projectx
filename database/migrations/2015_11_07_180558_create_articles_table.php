fc89019f35e7e41ff00078fc4b43b82bfdb4a10a
nobpo <thelast_chapter@hotmail.com>
2015-11-11 13:55:32 +0700

    Add new files

---
+++ database/migrations/2015_11_07_180558_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('subject_id');
+			$table->string('title');
+			$table->text('body1');
+			$table->text('body2');
+			$table->text('body3');
+			$table->text('body4');
+			$table->text('comment');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
---
+++ resources/views/page/detail.blade.php
@@ -0,0 +1,173 @@
+
+<html>
+
+    <head>
+   
+    
+     <title>ข้อมูลของ วิชา physic</title>
+
+      <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+      <style>
+            
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                color: #B0BEC5;
+                display: table;
+                background-image : url('http://localhost:8888/ProjectXX/public/images/nnnn.jpg');
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                margin-bottom: 40px;
+            }
+
+            .quote {
+                font-size: 24px;
+            }
+            .wrapper {
+                max-width: 100%;
+                margin: 0 auto;
+            }
+
+            @media screen and (min-width: 800px) {
+
+            .wrapper {
+             max-width: 90%;
+            }
+  
+            header {
+            width: 100%;
+            }
+  
+             nav, section {
+            float: left;
+            }
+
+              nav { 
+            width: 20.83333%;
+            margin-right: 1.041667%;
+              }
+
+              section {
+            width: 78.125%;
+            }
+  
+            }
+
+
+            @media screen and (min-width: 1000px) {
+
+            .wrapper {
+             max-width: 66.66667%;
+            }
+  
+            }
+
+
+
+
+
+            *, *:before, *:after {
+          -moz-box-sizing: border-box;
+          -webkit-box-sizing: border-box;   
+          box-sizing: border-box;
+            }
+
+        body {
+        background: #2980b9;
+        /*background-image : url('http://localhost:8888/ProjectXX/public/images/nnnn.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 100% 100%;
+        background-position: center;*/
+        color: #FFF;
+        font-family: Helvetica;
+        text-align: center;
+        margin: 0;
+        }
+
+        header,
+        nav,
+        section {
+        border: 1px solid rgba(255,255,255,0.8);
+        margin-bottom: 10px;
+        border-radius: 3px;
+        }
+
+        header {
+          padding: 20px 0;
+        }
+
+        nav, section {
+         padding: 100px 0;
+        }
+        </style>  
+
+      </head>
+
+<body>
+    
+      
+    <div class=""wrapper"">
+        <h1>ข้อมูลของรายวิชา</h1>
+        
+                    
+                    <header>PHYSIC</header>
+                    <nav>NAV</nav>
+                     <section>
+                        <b>ข้อมูลเพิ่มเติม:</b></p>
+                        <p>รายวิชาเกี่ยวกับวิชา ฟิสิก 1 เนื้อหาดังนี้</p>
+                        <b>ข้อมูลเพิ่มเติม:</b></p>
+                        <p>รายวิชาเกี่ยวกับวิชา ฟิสิก 1 เนื้อหาดังนี้</p>
+          
+                        <b>รีวิวจากรุ่นพี่:</b></p>
+                        <p>รายวิชานี้ยากมาก ถ้าเรียนไม่ไหวแนะนำให้ถอนแล้วไปเรียนในซัมเมอร์</p>
+           
+                        <b>เว็บไซต์:</b></p>
+                        <p>www.clipvidva.com</p>
+                        </p>
+
+
+
+                </section>
+                
+        </div>
+
+        
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Computer Science, Chulalongkorn University 2015</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- JavaScript -->
+
+    
+  
+  
+
+</body>
+
+</html>
